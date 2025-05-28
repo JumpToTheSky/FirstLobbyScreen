48b1ee2b1d3bb7526d15185447924b7341c9e864
Phạm Nguyễn <113409512+JumpToTheSky@users.noreply.github.com>
2025-05-28 23:55:13 +0700

    update

--- assets/Script/Loading/loadingPrefabController.js
+++ assets/Script/Loading/loadingPrefabController.js
@@ -13,29 +13,31 @@ cc.Class({
             type: cc.Label,
         },
     },
-    onLoad() {
-        console.log("Loading sceneLobby...");
 
-        this.loadingBar.progress = 0;
-        this.loadingLabel.string = "Loading...";
+    onLoad() {
         this.dotStates = ["Loading", "Loading.", "Loading..", "Loading..."];
         this.currentDotStateIndex = 0;
+    },
+
+    doLoadingScene(sceneName) {
+
+        this.loadingBar.progress = 0;
         this.loadingLabel.string = `${this.dotStates[this.currentDotStateIndex]} 0%`;
 
-        cc.director.preloadScene("sceneLobby", (completedCount, totalCount, item) => {
+        cc.director.preloadScene(sceneName, (completedCount, totalCount, item) => {
             console.log(`Preloading scene: ${item.url}`);
+            console.log(this.loadingBar.progress);
             let progress = totalCount > 0 ? completedCount / totalCount : 0;
             this.loadingBar.progress = progress;
 
-            let percentageText = `${Math.floor(progress * 100)}%`;
             let loadingPrefix = this.dotStates[this.currentDotStateIndex];
             this.loadingLabel.string = `${loadingPrefix} ${Math.floor(progress * 100)}%`;
-
             
             this.currentDotStateIndex = (this.currentDotStateIndex + 1) % this.dotStates.length;
         }, () => {
             cc.log("Scene preloaded successfully.");
-            cc.director.loadScene("sceneLobby");
+            cc.director.loadScene(sceneName);
+            this.node.destroy();
         });
     }
 
